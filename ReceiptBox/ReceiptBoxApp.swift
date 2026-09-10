08b975ae5ba9ef8231005258ead601da0bd00031
OrbitDevs <yoibuymac@proton.me>
2026-09-10 14:10:37 +0900

    Initial Commit

---
+++ ReceiptBox/ReceiptBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptBoxApp.swift
+//  ReceiptBox
+//
+//  Created by ys.kim's mac book neo  on 9/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReceiptBoxTests/ReceiptBoxTests.swift
@@ -0,0 +1,18 @@
+//
+//  ReceiptBoxTests.swift
+//  ReceiptBoxTests
+//
+//  Created by ys.kim's mac book neo  on 9/10/26.
+//
+
+import Testing
+
+struct ReceiptBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
